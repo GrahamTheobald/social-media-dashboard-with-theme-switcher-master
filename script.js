bdc9d33db43808e9bce74a6f806965e1145785cd
Graham Theobald <Graham_Theobald@live.co.uk>
2022-10-13 14:23:28 +0100

    mobile display

---
+++ script.js
@@ -0,0 +1,18 @@
+const THEMES = {
+    light: {
+        Toggle: '--Light-Toggle',
+        BG: '--White',
+        BGtop: '--VeryPaleBlue',
+        CardBG: '--LightGrayishBlue',
+        Text: '--DarkGrayishBlue',
+        Text2: '--VeryDarkBlue',
+    },
+    dark: {
+        Toggle: '--Dark-Toggle',
+        BG: '--VeryDarkBlue',
+        BGtop: '--VeryDarkBlue2',
+        CardBG: '--DarkDesaturatedBlue',
+        Text: '--DesaturatedBlue',
+        Text2: '--White',
+    }
+}
